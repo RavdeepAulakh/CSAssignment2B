5e5b5dba4f901efd62b4bb2dd57bd4ef6b41c0dd
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2023-11-28 15:56:24 -0800

    file name is being parsed and saved correctly, although file contents is still empty

--- src/UploadWebServer/UploadServlet.cpp
+++ src/UploadWebServer/UploadServlet.cpp
@@ -13,7 +13,7 @@
 #include <algorithm>
 
 
-const std::string DIRECTORY_PATH = "./images/";
+const std::string DIRECTORY_PATH = "/Users/laurieannesolkoski/CLionProjects/CSAssignment2B/images";
 const int PORT = 8083;
 const int BUFFER_SIZE = 16384;
 
@@ -46,48 +46,69 @@ void writeStringToSocket(int socket, const std::string &string) {
     send(socket, string.c_str(), string.size(), 0);
 }
 
+// Helper function to determine if a string starts with a given substring
+bool startsWith(const std::string &fullString, const std::string &starting) {
+    return fullString.compare(0, starting.size(), starting) == 0;
+}
 
 // Function to receive and parse HTTP request
 std::vector<std::string> receiveAndParseHttpRequest(int clientSocket) {
     debugLog("Receiving HTTP request");
+    std::string request;
     char buf;
-    std::string cur_str;
-    std::vector<std::string> data;
-    int i = 0;
-    char buf1[4] = {}; // Buffer to check for end of headers or payload
 
+    // Read the complete request (headers + potential beginning of the body)
     while (true) {
         ssize_t rval = read(clientSocket, &buf, 1);
         if (rval <= 0) {
-            // Handle read error or no data
             debugLog("Failed to receive data or connection closed");
             break;
         }
-
-        // Check for end of headers
-        buf1[i % 4] = buf;
-        if (i >= 3 && buf1[i % 4] == '\n' && buf1[(i - 1) % 4] == '\r' &&
-            buf1[(i - 2) % 4] == '\n' && buf1[(i - 3) % 4] == '\r') {
-            // Headers end detected
-            data.push_back(cur_str);
-            break;
+        request.push_back(buf);
+        if (request.size() >= 4 && request.substr(request.size() - 4) == "\r\n\r\n") {
+            break; // Headers end detected
         }
-
-        if (buf == '\r' || buf == '\n') {
-            if (!cur_str.empty()) {
-                data.push_back(cur_str);
-                cur_str.clear();
-            }
-        } else {
-            cur_str.push_back(buf);
-        }
-
-        i++;
     }
 
+    // Split the request into lines for initial processing
+    std::vector<std::string> data;
+    std::istringstream stream(request);
+    std::string line;
+    int contentLength = 0;
+
+    while (std::getline(stream, line)) {
+        if (!line.empty() && line.back() == '\r') {
+            line.pop_back(); // Remove carriage return character
+        }
+        data.push_back(line);
+        // Extract Content-Length
+        if (startsWith(line, "Content-Length:")) {
+            std::string lengthStr = line.substr(16); // Get substring after "Content-Length: "
+            contentLength = std::stoi(lengthStr); // Convert to integer
+        }
+    }
+
+    // Read the body of the request based on Content-Length
+    std::string body;
+    for (int i = 0; i < contentLength; ++i) {
+        if (read(clientSocket, &buf, 1) > 0) {
+            body.push_back(buf);
+        }
+    }
+
+    // Add body to data for further processing
+    std::istringstream bodyStream(body);
+    while (std::getline(bodyStream, line)) {
+        if (!line.empty() && line.back() == '\r') {
+            line.pop_back();
+        }
+        data.push_back(line);
+    }
 
     return data;
 }
+
+
 
 
 
@@ -116,129 +137,199 @@ void handleGET(int clientSocket) {
 }
 
 
-// Helper function to determine if a string starts with a given substring
-bool startsWith(const std::string &fullString, const std::string &starting) {
-    return fullString.compare(0, starting.size(), starting) == 0;
-}
-
-
 // Helper function to extract the boundary from the content type header
 std::string extractBoundary(const std::string &contentType) {
     std::string boundaryPrefix = "boundary=";
     size_t boundaryPos = contentType.find(boundaryPrefix);
     if (boundaryPos != std::string::npos) {
         boundaryPos += boundaryPrefix.length();
-        size_t endPos = contentType.length();
-        if (contentType[boundaryPos] == '"') {
-            boundaryPos++;
-            endPos = contentType.find('"', boundaryPos);
-        }
-        if (endPos == std::string::npos) {
-            endPos = contentType.length();
-        }
-        // The actual boundary is prefixed with "--"
-        return "--" + contentType.substr(boundaryPos, endPos - boundaryPos);
+        return "--" + contentType.substr(boundaryPos); // Ensure only the boundary is extracted
     }
     return "";
 }
 
 
+// Utility function to sanitize and generate a new filename
+std::string generateNewFilename(const std::string& caption, const std::string& date, const std::string& originalFilename) {
+    auto sanitize = [](const std::string& str) {
+        std::string result;
+        for (char c : str) {
+            if (c == ' ') result += "_";
+            else if (isalnum(c) || c == '_' || c == '-') result += c;
+        }
+        return result;
+    };
 
-// Function to handle POST request and save uploaded file
+    std::string sanitizedCaption = sanitize(caption);
+    std::string sanitizedDate = sanitize(date);
+    std::replace(sanitizedDate.begin(), sanitizedDate.end(), '-', '_');
+
+    std::string extension;
+    size_t dotPos = originalFilename.find_last_of(".");
+    if (dotPos != std::string::npos) {
+        extension = originalFilename.substr(dotPos);
+    }
+
+    if (!sanitizedCaption.empty() && !sanitizedDate.empty()) {
+        return sanitizedCaption + "_" + sanitizedDate + extension;
+    } else if (!sanitizedCaption.empty()) {
+        return sanitizedCaption + extension;
+    } else {
+        return "defaultFilename" + extension; // Replace with a default filename if both caption and date are empty
+    }
+}
+
+
+
+//parse the header
+void parseHeaders(const std::vector<std::string>& requestData, std::string& boundary, std::string& fieldName, std::string& fileName) {
+    for (const auto &line : requestData) {
+        if (startsWith(line, "Content-Type:")) {
+            boundary = extractBoundary(line);
+        } else if (startsWith(line, "Content-Disposition:")) {
+            size_t namePos = line.find("name=\"");
+            size_t filenamePos = line.find("filename=\"");
+
+            if (namePos != std::string::npos) {
+                namePos += 6; // Length of 'name="' is 6 characters
+                size_t endPos = line.find("\"", namePos);
+                if (endPos != std::string::npos) {
+                    fieldName = line.substr(namePos, endPos - namePos);
+                }
+                // Optionally, handle the case where endPos is std::string::npos
+            }
+
+            if (filenamePos != std::string::npos) {
+                filenamePos += 10; // Length of 'filename="' is 10 characters
+                size_t endPos = line.find("\"", filenamePos);
+                if (endPos != std::string::npos) {
+                    fileName = line.substr(filenamePos, endPos - filenamePos);
+                }
+                // Optionally, handle the case where endPos is std::string::npos
+            }
+        }
+    }
+}
+
+
+
+//read the binary data
+std::vector<char> readBinaryData(int clientSocket, const std::string& boundary) {
+    std::vector<char> fileContent;
+    std::string readBuffer;
+    char buf;
+    bool boundaryReached = false;
+
+    while (true) {
+        ssize_t rval = read(clientSocket, &buf, 1);
+        if (rval <= 0) {
+            // Handle errors or closed connection
+            break;
+        }
+
+        readBuffer.push_back(buf);
+
+        // Check if the read buffer ends with the boundary
+        if (readBuffer.size() >= boundary.size() &&
+            readBuffer.compare(readBuffer.size() - boundary.size(), boundary.size(), boundary) == 0) {
+            boundaryReached = true;
+            break;
+        }
+
+        // Append to file content
+        fileContent.push_back(buf);
+    }
+
+    // Remove the boundary from the end of file content
+    if (boundaryReached && fileContent.size() >= boundary.size()) {
+        fileContent.resize(fileContent.size() - boundary.size());
+    }
+
+    return fileContent;
+}
+
+
+
+
+
+//save the file function
+void saveFile(const std::string& directoryPath, const std::string& newFileName, const std::vector<char>& fileContent) {
+    std::string fullPath = directoryPath + newFileName;
+    std::ofstream outFile(fullPath, std::ios::binary);
+
+    if (outFile.is_open()) {
+        outFile.write(fileContent.data(), fileContent.size());
+        outFile.close();
+        debugLog("File saved: " + newFileName);
+    } else {
+        debugLog("Failed to open file for writing: " + newFileName);
+    }
+}
+
+//handle the post request
 void handlePostRequest(int clientSocket, std::vector<std::string> requestData) {
     debugLog("Entered handlePostRequest");
 
-    if (requestData.empty()) {
-        debugLog("Error: Request data is empty");
-        writeStringToSocket(clientSocket, "HTTP/1.1 400 Bad Request\r\n\r\n");
-        return;
-    }
 
-    debugLog("Starting to process request data");
-
-    // Find the Content-Type header to get the boundary
-    std::string boundary;
-    for (const auto &line : requestData) {
-        if (startsWith(line, "Content-Type:")) {
-            debugLog("Found Content-Type header: " + line);
-            boundary = extractBoundary(line);
-            debugLog("Extracted boundary: " + boundary);
-            if (!boundary.empty()) {
-                break;
-            }
-        }
-    }
-
-    if (boundary.empty()) {
-        debugLog("Error: Boundary not found in Content-Type header");
-        writeStringToSocket(clientSocket, "HTTP/1.1 400 Bad Request\r\n\r\n");
-        return;
-    }
-
-    debugLog("Processing multipart/form-data with boundary: " + boundary);
-
-
-    bool inContent = false;
-    std::string fieldName;
-    std::string filename;
+    std::string boundary, fieldName, fileName, caption, date;
+    std::string fieldValue;
     std::vector<char> fileContent;
-    std::string contentDisposition;
+    bool isReadingFile = false;
+    bool pastHeaders = false;
 
+// Extract boundary from the headers
     for (const auto &line : requestData) {
-        if (inContent) {
-            if (startsWith(line, boundary)) {
-                debugLog("Found boundary line, ending content section");
-                if (!filename.empty()) {
-                    debugLog("Saving file: " + filename);
-                    std::ofstream outFile(DIRECTORY_PATH + filename, std::ios::binary);
-                    if (outFile.is_open()) {
-                        outFile.write(fileContent.data(), fileContent.size());
-                        outFile.close();
-                        debugLog("File saved: " + filename);
-                    } else {
-                        debugLog("Failed to open file for writing: " + filename);
-                    }
-                }
-                inContent = false;
-                fileContent.clear();
-                filename.clear();
-            } else {
-                // Append to file content
-                fileContent.insert(fileContent.end(), line.begin(), line.end());
-                fileContent.push_back('\n'); // Append newline stripped by request parsing
-                debugLog("Appending data to file content buffer");
-            }
-        } else {
-            if (startsWith(line, "Content-Disposition:")) {
-                debugLog("Found Content-Disposition header: " + line);
-                contentDisposition = line;
-                size_t namePos = line.find("name=\"");
-                size_t filenamePos = line.find("filename=\"");
-
-                if (namePos != std::string::npos) {
-                    namePos += 6; // Length of 'name="'
-                    size_t endPos = line.find("\"", namePos);
-                    fieldName = line.substr(namePos, endPos - namePos);
-                    debugLog("Extracted field name: " + fieldName);
-                }
-
-                if (filenamePos != std::string::npos) {
-                    filenamePos += 10; // Length of 'filename="'
-                    size_t endPos = line.find("\"", filenamePos);
-                    filename = line.substr(filenamePos, endPos - filenamePos);
-                    debugLog("Extracted file name: " + filename);
-                }
-            } else if (line == "\r\n") {
-                // Start of content
-                inContent = !filename.empty();
-                debugLog("Starting to read file content for: " + filename);
-            }
+        if (startsWith(line, "Content-Type: multipart/form-data")) {
+            boundary = extractBoundary(line);
         }
+    }
+
+    // Process each line of request data
+    for (const auto &line : requestData) {
+        if (startsWith(line, boundary)) {
+            pastHeaders = false; // Reset for the next part of the form-data
+
+            if (!fieldName.empty() && isReadingFile && !fileContent.empty()) {
+                // End of file content reached
+                break;
+            } else if (!fieldValue.empty()) {
+                if (fieldName == "caption") caption = fieldValue;
+                else if (fieldName == "date") date = fieldValue;
+                fieldValue.clear(); // Clear the fieldValue for the next field
+            }
+
+            isReadingFile = false;
+        } else if (startsWith(line, "Content-Disposition:")) {
+            parseHeaders({line}, boundary, fieldName, fileName);
+            isReadingFile = (fieldName == "file");
+        } else if (isReadingFile) {
+            if (line == "\r\n" && !pastHeaders) {
+                pastHeaders = true; // Start reading the actual content after headers
+                continue;
+            }
+            if (pastHeaders) {
+                fileContent.insert(fileContent.end(), line.begin(), line.end());
+            }
+        } else if (!line.empty() && !isReadingFile) {
+            fieldValue += line; // Accumulate field value
+        }
+    }
+
+
+    // Generate filename and save file
+    if (!fileName.empty()) {
+        std::string newFileName = generateNewFilename(caption, date, fileName);
+        debugLog("Generated FileName: " + newFileName);
+        saveFile(DIRECTORY_PATH, "/" + newFileName, fileContent);
     }
 
     writeStringToSocket(clientSocket, "HTTP/1.1 200 OK\r\n\r\n");
     debugLog("POST handling complete");
 }
+
+
+
+
 
 
 
@@ -309,10 +400,14 @@ int main() {
     // Ensure the images directory exists
     struct stat st = {0};
     if (stat(DIRECTORY_PATH.c_str(), &st) == -1) {
-        mkdir(DIRECTORY_PATH.c_str(), 0700);
+        if (mkdir(DIRECTORY_PATH.c_str(), 0700) == -1) {
+            debugLog("Failed to create directory: " + DIRECTORY_PATH);
+            return 1; // or handle the error appropriately
+        }
     }
 
     startServer();
     debugLog("Server shutdown");
     return 0;
 }
+
